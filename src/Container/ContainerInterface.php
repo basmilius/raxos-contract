6f5c7735e34e9396adc2391a075136dcc44ea8b9
Bas Milius <basmilius@hotmail.com>
2026-05-24 22:22:36 +0200

    Add new exceptions, attributes, and PSR-11 container support
    Introduced NotFoundException, InvalidCallableException, and attribute Env for enhanced error handling and dependency injection capabilities. Implemented support for PSR-11 with PsrContainerAdapter. Extended ContainerInterface and Container classes with new methods and refinements for improved autowiring, instance registration, and tagged dependencies.

--- src/Container/ContainerInterface.php
+++ src/Container/ContainerInterface.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Raxos\Contract\Container;
 
+use Closure;
 use UnitEnum;
 
 /**
@@ -40,6 +41,42 @@ interface ContainerInterface
     public function bindIf(string $abstract, callable|string|null $concrete): void;
 
     /**
+     * Calls the given callable, autowiring any unprovided parameters via the container.
+     *
+     * Supported callable forms:
+     *  - {@see Closure}
+     *  - [class-string, method-name]
+     *  - [object, method-name]
+     *  - invokable class-string (must define {@see __invoke})
+     *
+     * Values in `$args` are matched by parameter name and override autowired values.
+     *
+     * @param Closure|array|string $callable
+     * @param array<string, mixed> $args
+     *
+     * @return mixed
+     * @throws ContainerExceptionInterface
+     * @author Bas Milius <bas@mili.us>
+     * @since 2.2.0
+     */
+    public function call(Closure|array|string $callable, array $args = []): mixed;
+
+    /**
+     * Registers an existing instance as a singleton in the container.
+     *
+     * @template TClass of object
+     *
+     * @param class-string<TClass>|string $abstract
+     * @param TClass $instance
+     * @param UnitEnum|string|null $tag
+     *
+     * @return void
+     * @author Bas Milius <bas@mili.us>
+     * @since 2.2.0
+     */
+    public function instance(string $abstract, object $instance, UnitEnum|string|null $tag = null): void;
+
+    /**
      * Adds a singleton binding to the container.
      *
      * @param string $abstract
@@ -64,6 +101,18 @@ interface ContainerInterface
      * @since 2.0.0
      */
     public function singletonIf(string $abstract, callable|string|null $concrete, UnitEnum|string|null $tag = null): void;
+
+    /**
+     * Returns all resolved singletons that share the given tag, keyed by their abstract.
+     *
+     * @param UnitEnum|string $tag
+     *
+     * @return iterable<string, object>
+     * @throws ContainerExceptionInterface
+     * @author Bas Milius <bas@mili.us>
+     * @since 2.2.0
+     */
+    public function tagged(UnitEnum|string $tag): iterable;
 
     /**
      * Unbinds a definition from the container.
